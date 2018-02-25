77797179a2ab4662342521bb41802f307867cf7b
Dalmiro Granas <dalmiro.granias@gmail.com>
2018-02-25 16:52:01 -0300

    adding base tests

---
+++ source/Pushover.Client.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Pushover.Client.Tests
+{
+    [TestFixture]
+    public class MoneyTest
+    {
+        [Test]
+        public void JustPass()
+        {
+            Assert.Equals(1, 1);
+        }
+    }
+}
